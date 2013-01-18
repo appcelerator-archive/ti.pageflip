9e7af7093b7595035762a84cac3851d8f49b0b56
Jon Alter <jon@local>
2013-01-18 10:20:42 -0800

    [TIMODOPEN-213] Fixing Copyright in Source

--- ios/Classes/PDFSource.m
+++ ios/Classes/PDFSource.m
@@ -1,7 +1,6 @@
 /**
- * Appcelerator Titanium Mobile
- * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
+ * Ti.Pageflip Module
+ * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
  * Please see the LICENSE included with this distribution for details.
  */
 
--- ios/Classes/SlideTransition.h
+++ ios/Classes/SlideTransition.h
@@ -1,7 +1,6 @@
 /**
- * Appcelerator Titanium Mobile
+ * Ti.Pageflip Module
  * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
 
